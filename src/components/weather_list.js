a2d2d14df35cdd86c92617d9b7f2a16176ae8e09
alyson-b69 <alyson.bernabeu@gmail.com>
2020-12-03 14:52:30 +0100

    delete h1 on weatherList - it was just for a test

--- src/components/weather_list.js
+++ src/components/weather_list.js
@@ -19,7 +19,6 @@ const WeatherList = (props) => {
 
   return (
     <section className="weatherContainer">
-      <h1>Meteo</h1>
       {props.weather.map((cityData) => {
         return (
           <div className="mt-3 mb-2">
